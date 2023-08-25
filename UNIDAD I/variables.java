845d0cac9b53ae9944b85418ed6e70b0cc11d34a
Goncar2023 <eliana.gonzalez@edu.ipchile.cl>
2023-08-24 22:38:20 -0400

    Elementos básicos de Java

---
+++ UNIDAD I/variables.java
@@ -0,0 +1,39 @@
+/*
+    IMPORTANTE
+    Para ejecutar el ejemplo:
+    -> Crea un nuevo proyecto en NetBeans
+    -> Copia y pega el código que se encuentra dentro de la clase main de este archivo y
+        pegalo dentro de la clase main de tu proyecto.
+*/
+public static void main(String[] args) {
+    //Declaración de una variable de tipo String
+    String nombre;
+    //Asignación de un valor
+    nombre = "Hugo Cortés";
+    //Imprimir el valor de la variable por consola
+    System.out.println("El nombre es: "+nombre);
+    //Para CONCATENAR cadenas y variables se utiliza el operador + 
+
+    //Declaración y asignacion de una variable String
+    String nombreUsuario = "Hugo2023";
+    
+    /*
+        TIPOS DE VARIABLES
+    */
+    
+    //NUMÉRICAS ENTERAS -> Almacenan números enteros sin parte decimal
+    int edad = 20;
+    long masa = 16456521L; //La L indica que es de tipo long
+    
+    //NUMÉRICAS DE PUNTO FLOTANTE
+    float decimal = 1.75f; //La F indica que es de tipo float
+    double pi = 3.14159265359;
+    
+    //CARÁCTER
+    char letra = 'A';
+
+    //BOOLEANO -> Solo puede ser Verdadero o Falso
+    boolean esMenor = true;
+    boolean isActive = false;
+}
+
